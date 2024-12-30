eb77864e2916221320391869ee9b9defa8fc0823
Rahul Sutradhar <rahulsd380@gmail.com>
2024-12-30 20:14:12 +0600

    Responsiveness added for shop card

--- src/app/(mainLayout)/shops/page.tsx
+++ src/app/(mainLayout)/shops/page.tsx
@@ -5,13 +5,13 @@ import ShopCard from "@/components/Shops/ShopCard/ShopCard";
 const Shops = () => {
     return (
         <Container>
-            <h1 className="text-neutral-15 font-Sora text-[40px] md:text-[45px] font-semibold mt-[50px]">
-          All Shops You May Follow
+            <h1 className="text-neutral-15 font-Sora text-[40px] md:text-[45px] font-semibold mt-9">
+          All Shops You <span className="text-primary-10">May Follow</span>
         </h1>
-        <p className="text-neutral-60 font-Inter font-medium">
+        <p className="text-neutral-60 font-Inter font-medium max-w-[1000px]">
           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Possimus iure ad necessitatibus eaque! Ducimus doloribus enim veniam necessitatibus perspiciatis, quia asperiores molestiae, culpa eos eius numquam iste soluta magnam impedit.
         </p>
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-10  mt-8">
+            <div className="grid grid-cols-1 lg:grid-cols-2 gap-10  mt-10">
                 <ShopCard />
             </div>
         </Container>
--- src/components/Shops/ShopCard/ShopCard.tsx
+++ src/components/Shops/ShopCard/ShopCard.tsx
@@ -8,7 +8,7 @@ const ShopCard = () => {
     return (
         <div className='bg-gradient-to-r from-slate-50/70 to-green-50/70 border border-primary-10/30 rounded-2xl p-6'>
 
-            <div className='flex items-center justify-between'>
+            <div className='flex flex-col md:flex-row gap-4 md:gap-0 items-start md:items-center justify-between'>
                 <div className="flex items-center gap-3">
                     <div className="size-16 rounded-full border border-primary-10 bg-white flex items-center justify-center">
                         <div className='bg-[#FEEFEA] p-1 flex items-center justify-center rounded-full size-[55px]'>
@@ -55,14 +55,14 @@ const ShopCard = () => {
 
             <div className="flex items-center gap-4 mt-7">
                 <button
-                    className="border border-primary-10 hover:bg-primary-10 transition duration-300 hover:text-white px-4 py-3 text-primary-10 font-medium font-Sora flex items-center gap-3 rounded-md w-fit"
+                    className="border border-primary-10 hover:bg-primary-10 transition duration-300 hover:text-white px-2 md:px-4 py-2 md:py-3 text-primary-10 font-medium font-Sora rounded-md w-full md:w-fit text-sm md:text-base text-center"
                 >
                     Follow Now
                 </button>
 
                 <Link
-                    href={"/signup"}
-                    className="bg-primary-10 px-4 py-3 text-white font-medium font-Sora flex items-center gap-3 rounded-md w-fit"
+                    href={`seller/${1}`}
+                    className="bg-primary-10 px-2 md:px-4 py-2 md:py-3 text-white font-medium font-Sora flex items-center justify-center gap-3 rounded-md w-full md:w-fit text-sm md:text-base"
                 >
                     View Profile
                     <Image
